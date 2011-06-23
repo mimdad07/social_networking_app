79be974a2e6fa819adf36a1decc8845301a810f7
Imdad Sarkar <mimdad07@gmail.com>
2011-06-23 17:15:19 +0600

    Splash logo, login and signup window has been introduced.

--- gen/created/by/imdad/alapdemo/R.java
+++ gen/created/by/imdad/alapdemo/R.java
@@ -8,68 +8,80 @@
 package created.by.imdad.alapdemo;
 
 public final class R {
+    public static final class anim {
+        public static final int clear=0x7f040000;
+        public static final int in_anim=0x7f040001;
+        public static final int in_popup=0x7f040002;
+        public static final int left_in=0x7f040003;
+        public static final int left_out=0x7f040004;
+        public static final int logo_in=0x7f040005;
+        public static final int out_anim=0x7f040006;
+        public static final int out_popup=0x7f040007;
+        public static final int right_in=0x7f040008;
+        public static final int right_out=0x7f040009;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int colors=0x7f040004;
-        public static final int notepad_lines=0x7f040001;
-        public static final int notepad_margin=0x7f040002;
-        public static final int notepad_paper=0x7f040000;
-        public static final int notepad_text=0x7f040003;
+        public static final int colors=0x7f050004;
+        public static final int notepad_lines=0x7f050001;
+        public static final int notepad_margin=0x7f050002;
+        public static final int notepad_paper=0x7f050000;
+        public static final int notepad_text=0x7f050003;
     }
     public static final class dimen {
-        public static final int notepad_margin=0x7f050000;
+        public static final int notepad_margin=0x7f060000;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int address=0x7f08001d;
-        public static final int autoLogin=0x7f080009;
-        public static final int btn_event_back=0x7f080018;
-        public static final int btn_event_friendlist=0x7f080014;
-        public static final int btn_event_notification=0x7f080017;
-        public static final int btn_event_pendingfriendlist=0x7f080016;
-        public static final int btn_event_requestingfriendlist=0x7f080015;
-        public static final int createanewaccount=0x7f08000a;
-        public static final int flipFirst=0x7f080000;
-        public static final int forgateusername=0x7f08000b;
-        public static final int friends_tab_list=0x7f08001a;
-        public static final int fullname=0x7f08001c;
-        public static final int hobby=0x7f080022;
-        public static final int login=0x7f080008;
-        public static final int mobileno=0x7f080021;
-        public static final int religion=0x7f080023;
-        public static final int savesetting=0x7f080025;
-        public static final int sc=0x7f08001b;
-        public static final int search=0x7f08002a;
-        public static final int search_scrvw=0x7f080019;
-        public static final int search_tab_list=0x7f08002b;
-        public static final int search_table=0x7f080026;
-        public static final int settings=0x7f08000c;
-        public static final int signup_email=0x7f080010;
-        public static final int signup_password=0x7f08000e;
-        public static final int signup_repassword=0x7f08000f;
-        public static final int signup_username=0x7f08000d;
-        public static final int signupinfo=0x7f080011;
-        public static final int spnr_set_age=0x7f08001f;
-        public static final int spnr_set_country=0x7f08001e;
-        public static final int spnr_set_sex=0x7f080020;
-        public static final int spnr_set_workfld=0x7f080024;
-        public static final int spnr_srch_age=0x7f080028;
-        public static final int spnr_srch_country=0x7f080029;
-        public static final int spnr_srch_sex=0x7f080027;
-        public static final int start_progressbar=0x7f080001;
-        public static final int start_progressbar1=0x7f080012;
-        public static final int tableLayout1=0x7f080002;
-        public static final int tableRow1=0x7f080003;
-        public static final int tableRow2=0x7f080006;
-        public static final int textView1=0x7f080004;
-        public static final int txtPassword=0x7f080007;
-        public static final int txtUserName=0x7f080005;
-        public static final int view1=0x7f08002c;
-        public static final int welcome=0x7f080013;
+        public static final int address=0x7f09001d;
+        public static final int autoLogin=0x7f090009;
+        public static final int btn_event_back=0x7f090018;
+        public static final int btn_event_friendlist=0x7f090014;
+        public static final int btn_event_notification=0x7f090017;
+        public static final int btn_event_pendingfriendlist=0x7f090016;
+        public static final int btn_event_requestingfriendlist=0x7f090015;
+        public static final int createanewaccount=0x7f09000a;
+        public static final int flipFirst=0x7f090000;
+        public static final int forgateusername=0x7f09000b;
+        public static final int friends_tab_list=0x7f09001a;
+        public static final int fullname=0x7f09001c;
+        public static final int hobby=0x7f090022;
+        public static final int login=0x7f090008;
+        public static final int mobileno=0x7f090021;
+        public static final int religion=0x7f090023;
+        public static final int savesetting=0x7f090025;
+        public static final int sc=0x7f09001b;
+        public static final int search=0x7f09002a;
+        public static final int search_scrvw=0x7f090019;
+        public static final int search_tab_list=0x7f09002b;
+        public static final int search_table=0x7f090026;
+        public static final int settings=0x7f09000c;
+        public static final int signup_email=0x7f090010;
+        public static final int signup_password=0x7f09000e;
+        public static final int signup_repassword=0x7f09000f;
+        public static final int signup_username=0x7f09000d;
+        public static final int signupinfo=0x7f090011;
+        public static final int spnr_set_age=0x7f09001f;
+        public static final int spnr_set_country=0x7f09001e;
+        public static final int spnr_set_sex=0x7f090020;
+        public static final int spnr_set_workfld=0x7f090024;
+        public static final int spnr_srch_age=0x7f090028;
+        public static final int spnr_srch_country=0x7f090029;
+        public static final int spnr_srch_sex=0x7f090027;
+        public static final int start_progressbar=0x7f090001;
+        public static final int start_progressbar1=0x7f090012;
+        public static final int tableLayout1=0x7f090002;
+        public static final int tableRow1=0x7f090003;
+        public static final int tableRow2=0x7f090006;
+        public static final int textView1=0x7f090004;
+        public static final int txtPassword=0x7f090007;
+        public static final int txtUserName=0x7f090005;
+        public static final int view1=0x7f09002c;
+        public static final int welcome=0x7f090013;
     }
     public static final class layout {
         public static final int firstlayout=0x7f030000;
@@ -80,11 +92,11 @@ public final class R {
         public static final int tabs=0x7f030005;
     }
     public static final class string {
-        public static final int about=0x7f060002;
-        public static final int app_name=0x7f060001;
-        public static final int hello=0x7f060000;
+        public static final int about=0x7f070002;
+        public static final int app_name=0x7f070001;
+        public static final int hello=0x7f070000;
     }
     public static final class style {
-        public static final int ExitDialogAnimation=0x7f070000;
+        public static final int ExitDialogAnimation=0x7f080000;
     }
 }
--- src/created/by/imdad/alapdemo/Alap.java
+++ src/created/by/imdad/alapdemo/Alap.java
@@ -4,16 +4,230 @@ package created.by.imdad.alapdemo;
 import created.by.imdad.alapdemo.tab.Tab;
 
 import android.app.Activity; 
-import android.content.Intent; 
-import android.os.Bundle;   
-public class Alap extends Activity 
-{	  
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+public class Alap extends Activity implements OnClickListener, OnTouchListener
+{	 
+	private int progress=0, run=0, range =100000,last = 32768, which=1;
+	ProgressBar progressBar, progressBar1; 
+
+	Button  login, 
+			signup_info, 
+			welcome;
+	TextView create_a_new_account, 
+			 forgate_username; 
+	 
+	CheckBox autologin;
+	
+	public static final String PREF_FILENAME = "AlapPreference"; 
+	SharedPreferences preference;
+	
+	String 	sendUsernameAndPassword = "send_username_and_password.php",
+			myIpPage= "my_ip.php";
+	Thread th;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{	
 		super.onCreate(savedInstanceState);
+		setFullScreen();	 
+		setContentView( R.layout.firstlayout ); 
+		progressBar = (ProgressBar) findViewById( R.id.start_progressbar );
+		progressBar1 = (ProgressBar) findViewById( R.id.start_progressbar1 );
+		
+		login = (Button ) findViewById( R.id.login );
+		signup_info = (Button ) findViewById( R.id.signupinfo ); 
+		welcome = (Button ) findViewById( R.id.welcome );
+		autologin = ( CheckBox ) findViewById( R.id.autoLogin );
+		create_a_new_account = (TextView ) findViewById( R.id.createanewaccount );
+		forgate_username = (TextView ) findViewById( R.id.forgateusername ); 
+		
+		login.setOnClickListener( this );
+		create_a_new_account.setOnClickListener( this );
+		forgate_username.setOnClickListener( this );
+		signup_info.setOnClickListener( this );
+		welcome.setOnClickListener( this );
+		
+		create_a_new_account.setOnTouchListener(this);
+		forgate_username.setOnTouchListener(this);
+		
+		EditText username = (EditText)findViewById( R.id.txtUserName);
+		EditText password = (EditText)findViewById( R.id.txtPassword);
 		 
-		Intent intent = new Intent( this, Tab.class );
-		startActivity( intent );
-	} 
+		renderProgressBar(progressBar  );
+		 
+	}
+	
+	@Override
+	protected void onPause() 
+	{
+		super.onPause();
+		finish();
+	}
+ 
+	public void renderProgressBar(ProgressBar p )
+    {
+		final ProgressBar progressBar=p;
+		th = new Thread( new Runnable()
+        {
+			@Override
+			public void run()
+    		{
+    			while( run<range )
+    			{
+    				try
+    				{
+    					handle.sendMessage( handle.obtainMessage());
+    					Thread.sleep(10);
+    				}
+    				catch( Throwable t){}   				  				   			
+    			}   			 
+    		}  
+    	
+    		Handler handle = new Handler()
+    		{
+				@Override
+				public void handleMessage( Message msg)
+    			{
+					run++;
+					if( run>=200)
+						progress++;
+					
+					if( run==200)
+					{
+						ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+						
+						vf.showNext();
+					}
+    				
+                   if( progress<=100)
+    				progressBar.setProgress(progress);
+    				
+    				if( progress == 20 &&  !autologin.isChecked())
+    				{
+    					ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+    					if( which ==1 )
+    					{
+    						vf.setInAnimation(AnimationUtils.loadAnimation( getApplicationContext(), R.anim.right_in ));
+    						vf.setOutAnimation( AnimationUtils.loadAnimation( getApplicationContext(), R.anim.left_out ));
+    					}
+    					else if(which==2)
+    					{
+    					}
+    					vf.showNext();
+    				}
+    				
+    				if( progress == 20 &&  autologin.isChecked())
+    				{   					
+    					ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+    					vf.showNext();
+    					vf.setAnimation(AnimationUtils.loadAnimation( getApplicationContext(), R.anim.in_anim));   					
+    					vf.showNext();
+    				}
+    				
+    				if( (last+progress) == 100 )
+    				{
+    				
+    					ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+    					vf.setAnimation(AnimationUtils.loadAnimation( getApplicationContext(), R.anim.clear));    					
+    					vf.showNext();   				
+    				}  			
+    			}   		
+    		};
+    	});
+		th.start();  
+    }
+		
+	@Override
+	public boolean onTouch(View view, MotionEvent action) 
+	{
+		if( action.getAction()== MotionEvent.ACTION_UP ) //avoiding down listener
+		{
+			if( view == findViewById( R.id.createanewaccount ))
+			{
+				ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+				vf.setInAnimation( AnimationUtils.loadAnimation(getApplicationContext(), R.anim.in_popup));
+				vf.setOutAnimation( AnimationUtils.loadAnimation(getApplicationContext(), R.anim.out_popup));
+				
+				vf.showNext();
+			}
+			 
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public void onClick(View view) 
+	{
+		if( view == findViewById( R.id.login ))
+		{
+			ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+			
+			EditText username = (EditText)findViewById( R.id.txtUserName);
+			EditText password = (EditText)findViewById( R.id.txtPassword);
+			CheckBox checkBox = (CheckBox) findViewById(R.id.autoLogin);
+			 
+		}
+		
+		
+		
+		
+		if( view == findViewById( R.id.signupinfo ))
+		{
+			ViewFlipper vf = (ViewFlipper) findViewById( R.id.flipFirst);
+			vf.setInAnimation(AnimationUtils.loadAnimation( getApplicationContext(), R.anim.left_in));
+			vf.setOutAnimation(AnimationUtils.loadAnimation( getApplicationContext(), R.anim.out_popup));
+
+			EditText   username,
+					   password,
+					   repassword,
+					   email;
+			
+			username = (EditText)findViewById( R.id.signup_username);
+			password = (EditText)findViewById( R.id.signup_password);
+			repassword = (EditText)findViewById( R.id.signup_repassword);
+			email =  (EditText)findViewById( R.id.signup_email );
+			
+			
+			//required for storing ip address.
+			String data[][] = new String[2][2];			
+			data[0][0] = "user_name";			data[0][1] = username.getText().toString().trim();
+			data[1][0] = "user_password";		data[1][1] = password.getText().toString().trim();
+			 
+		}
+		
+		//---------latter i will delete this
+		if( view == findViewById( R.id.welcome ))
+		{
+			Intent intent = new Intent( this, Tab.class );
+			startActivity( intent );
+		}
+		
+	}
+	
+	
+	 public void setFullScreen()
+	 {
+	    	this.getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+	        this.requestWindowFeature(Window.FEATURE_NO_TITLE);  
+	    	
+	 } 
 }
